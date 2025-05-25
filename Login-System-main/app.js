57d67b741b7f810e56429dbb47cfdd4848f90e95
HuseyinSoykok <huseyinsoykok001@gmail.com>
2025-05-25 20:33:39 +0200

    First Setup

---
+++ Login-System-main/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import bodyParser from "body-parser";
+import passport from "passport";
+import { Strategy } from "passport-local";
+import GoogleStrategy from "passport-google-oauth2";
+import session from "express-session";
+
+import router from "./router/router.js"
+
+const app=express();
+const port=3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET || "dev-secret-key-2024-05-23-!@#Unique$%^&*()_+",
+        resave: false,
+        saveUninitialized: true,
+    })
+)
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/user",router);
+
+app.listen(port,() => {
+    console.log(`server is listening at http://localhost:${port}/user`);
+})
