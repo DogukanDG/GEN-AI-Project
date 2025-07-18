9eeaac9a26067b3c69e8a7cab562d45acced0e32
Mustafa Mengutay <48598974+mustafamengutay@users.noreply.github.com>
2025-07-18 20:39:40 +0200

    feat: add admin dashboard apis

--- server/src/modules/user/user.repository.ts
+++ server/src/modules/user/user.repository.ts
@@ -10,11 +10,11 @@ export async function createUser(data: {
   role?: string;
 }): Promise<User> {
   try {
-    return await prisma.user.create({ 
+    return await prisma.user.create({
       data: {
         ...data,
-        role: data.role || 'normal'
-      }
+        role: data.role || 'normal',
+      },
     });
   } catch {
     throw new HttpError(500, 'User could not be created');
@@ -26,5 +26,40 @@ export async function findByEmail(email: string): Promise<User | null> {
     return await prisma.user.findUnique({ where: { email } });
   } catch {
     throw new HttpError(500, 'Data could not be fetched');
+  }
+}
+
+export async function findAllUsers(): Promise<User[]> {
+  try {
+    return await prisma.user.findMany();
+  } catch {
+    throw new HttpError(500, 'Users could not be fetched');
+  }
+}
+
+export async function findUserById(id: number): Promise<User | null> {
+  try {
+    return await prisma.user.findUnique({ where: { id } });
+  } catch {
+    throw new HttpError(500, 'User could not be fetched');
+  }
+}
+
+export async function updateUser(
+  id: number,
+  data: Partial<Omit<User, 'id'>>
+): Promise<User> {
+  try {
+    return await prisma.user.update({ where: { id }, data });
+  } catch {
+    throw new HttpError(500, 'User could not be updated');
+  }
+}
+
+export async function deleteUser(id: number): Promise<User> {
+  try {
+    return await prisma.user.delete({ where: { id } });
+  } catch {
+    throw new HttpError(500, 'User could not be deleted');
   }
 }
